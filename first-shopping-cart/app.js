cfd2bac0379235b0b4dd629e78b9ad3381170d8d
Pitt Wu <kgb00128@gmail.com>
2024-09-07 21:28:21 +0800

    [feature][shopping cart] clean code shopping cart

--- first-shopping-cart/app.js
+++ first-shopping-cart/app.js
@@ -1,80 +1,62 @@
-// 提示：修改為有意義且可閱讀的變數名稱
-let cart = [{
-	id: 0,
-	i: 'Orange',
-	p: 2,
-	q: 1
-}];
+const cart = [
+  {
+    id: 0,
+    name: 'Orange',
+    price: 2,
+    quantity: 1,
+  },
+];
 
-// 提示：修改為有意義且可閱讀的變數名稱
-// 提示：參數少於 2 個較佳
-function add(item, price, quantity) {
-		const id = Date.now();
-    cart.push({ id: id, i: item, p: price, q: quantity });
-}
-// add('Banana',1,1);
+const addItem = (item) => {
+  const newItem = {
+    id: Date.now(),
+    ...item,
+  };
+  cart.push(newItem);
+};
 
-
-// 提示：修改為有意義且可閱讀的變數名稱
-// 提示：參數少於 2 個較佳
-// 提示：進階寫法可以用 findIndex 語法（非必做）
-function edit(id, newItem, newPrice, newQuantity) { 
-  for (var i = 0; i < cart.length; i++) {
-    if (cart[i].id === id) {
-      cart[i].i = newItem;
-      cart[i].p = newPrice;
-      cart[i].q = newQuantity;
-      break;
-    }
+/**
+  1. 使用 findIndex 找出要更新的商品的索引
+  2. 使用展開運算符，將更新的商品覆寫到原始商品的屬性
+**/
+const editItem = (id, updateItem) => {
+  const itemIndex = cart.findIndex((item) => item.id === id);
+  if (itemIndex !== -1) {
+    cart[itemIndex] = { ...cart[itemIndex], ...updateItem };
   }
-}
-// edit(0,'Strawberry',3,4);
+};
 
-
-// 提示：修改為有意義且可閱讀的變數名稱
-// 提示：條件判斷邏輯複雜，可再整理得更清楚
-// 提示：函式一次只做一件事
-// 提示：進階寫法可以用 findIndex 語法（非必做）
-function remove(id) {
-  if (id) {
-    for (var i = 0; i < cart.length; i++) {
-        if (cart[i].id === id) {
-            cart.splice(i, 1);
-            break;
-        }
-    }
-  } else {
+const removeItem = (id) => {
+  if (!id) {
     cart = [];
+    return;
   }
-}
 
-// 提示：修改為有意義且可閱讀的變數名稱
-// 提示：進階寫法可以用 reduce 語法（非必做）
-function calcTotal() {
-    var t = 0;
-    for (var i = 0; i < cart.length; i++) {
-        t += cart[i].p * cart[i].q;;
-    }
-    return t;
-}
+  const itemIndex = cart.findIndex((item) => item.id === id);
+  if (itemIndex !== -1) {
+    cart.splice(itemIndex, 1);
+  }
+};
 
-// 提示：修改為有意義且可閱讀的變數名稱
-let loggedIn = false;
+const calculateTotal = () => {
+  return cart.reduce((total, item) => total + item.price * item.quantity, 0);
+};
 
-// 提示：條件判斷邏輯複雜（太多的 if/else），可再整理得更清楚
-function checkout() {
-    if (cart.length === 0) {
-        return '您的購物車是空的';
-    } else {
-        if (loggedIn) {
-            // 提示：使用可搜尋的名稱
-            if (cart.length > 5) {
-                return '您超出了允許結帳的最大商品數量';
-            } else {      
-                return '付款成功';
-            }
-        } else {
-            return '請先登入';
-        }
-    }
-}
+let isLoggedIn = false;
+const MAX_ITEMS_FOR_CHECKOUT = 5;
+
+const checkout = () => {
+  if (cart.length === 0) {
+    return '您的購物車是空的';
+  }
+
+  if (!isLoggedIn) {
+    return '請先登入';
+  }
+
+  if (cart.length > MAX_ITEMS_FOR_CHECKOUT) {
+    return '您超出了允許結帳的最大商品數量';
+  }
+
+  return '付款成功';
+};
